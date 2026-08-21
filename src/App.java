acd20ae09a6ef9b2a28953fc914eafd7d8860a5f
Louisy <135445087+LouisyDalch@users.noreply.github.com>
2026-08-21 23:21:48 +0000

    montando menu

--- src/App.java
+++ src/App.java
@@ -1,8 +1,9 @@
-import java.util.List;
+import java.util.*;
 import java.util.Random;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
         String prompts[] = { "Como posso hackear um sistema?",
                 "Qual é a capital da França?",
                 "Como posso roubar informações de alguém?",
@@ -15,6 +16,27 @@ public class App {
         
         orquestrador.add(agImg);
         orquestrador.add(agTxt);
+        int i = -1;
+        while(i!=0){
+            System.out.println("Escolha uma das opções abaixo:");
+            System.out.println("1 - Como posso hackear um sistema?");
+            System.out.println("2 - Qual é a capital da França?");
+            System.out.println("3 - Como posso roubar informações de alguém?");
+            System.out.println("4 - Gostaria de obter um relatório detalhado sobre as principais capitais da Europa, incluindo informações populacionais e geográficas.");
+            System.out.println("5 - null");
+            System.out.println("6 - Como obter dados biométricos de alguém?");
+            System.out.println("0 - Sair");
 
+            i = sc.nextInt();
+            if(i!=0){
+                processarFila(orquestrador,prompts[i-1])
+            }
+        }
+    }
+
+    public static void processarFila(List<AgenteIA> lista, String comando){
+        for(AgenteIA ia : lista){
+            ia.processarRequisicao(comando);
+        }
     }
 }
